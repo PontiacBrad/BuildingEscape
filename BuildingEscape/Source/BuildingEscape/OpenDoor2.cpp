9fff978f6d477752323ca0759af2dda1615b5b80
PontiacBrad <bradley.gray@stu.laroche.edu>
2018-08-02 22:03:32 -0400

    Layed out first room

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor2.cpp
@@ -0,0 +1,41 @@
+// Copyright Bradley Gray 2018
+
+#include "BuildingEscape.h"
+#include "OpenDoor2.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor2::UOpenDoor2()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor2::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//Find the owning actor
+	AActor *Owner = GetOwner();
+
+	//Create a rotation
+	FRotator NewRotation = FRotator(0.0f, 0.0f, 0.0f);
+
+	//Set the door rotation
+	Owner->SetActorRotation(NewRotation);
+}
+
+
+// Called every frame
+void UOpenDoor2::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor2.h
@@ -0,0 +1,28 @@
+// Copyright Bradley Gray 2018
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "OpenDoor2.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UOpenDoor2 : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor2();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+	
+};
